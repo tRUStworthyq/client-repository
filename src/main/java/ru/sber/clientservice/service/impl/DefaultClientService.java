7c7f7e4ab44eeeade32df5f582cb551026083f48
Maxim <dolgiy_m@list.ru>
2026-04-17 17:33:59 +0300

    feat: added service layer

---
+++ src/main/java/ru/sber/clientservice/service/impl/DefaultClientService.java
@@ -0,0 +1,36 @@
+package ru.sber.clientservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.sber.clientservice.entity.Client;
+import ru.sber.clientservice.exception.ClientNotFoundException;
+import ru.sber.clientservice.repository.ClientRepository;
+import ru.sber.clientservice.service.ClientService;
+import ru.sber.dto.ClientResponseDto;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DefaultClientService implements ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public ClientResponseDto getClientByDealId(String dealId) {
+        log.info("Fetching client info for dealId: {}", dealId);
+        Client client = clientRepository.findByDealId(dealId)
+                .orElseThrow(() -> new ClientNotFoundException("Client not found for dealId: " + dealId));
+        return toDto(client);
+    }
+
+    private ClientResponseDto toDto(Client client) {
+        return ClientResponseDto.builder()
+                .dealId(client.getDealId())
+                .fullName(client.getFullName())
+                .inn(client.getInn())
+                .build();
+    }
+}
